b06dd17cecf833237832ae477d59da6a9fe1bf3a
Keith Smiley <k@keith.so>
2017-07-27 11:40:48 -0700

    Update simulated metrics rule to output correct line
    Previously we were printing this error as if it was on the root node,
    rather than the actual line of the simulated metrics node.

--- xiblint/rules/simulated_metrics_retina4_0.py
+++ xiblint/rules/simulated_metrics_retina4_0.py
@@ -7,4 +7,4 @@ def check(context):  # type: (xiblint.xibcontext.XibContext) -> None
     root = context.tree.getroot()
     for simulated_screen_metrics in root.findall(".//simulatedScreenMetrics[@key='destination']"):
         if simulated_screen_metrics.get('type') != 'retina4_0.fullscreen':
-            context.error(root, 'Simulated metrics ("View As:") must be set to iPhone SE. ')
+            context.error(simulated_screen_metrics, 'Simulated metrics ("View As:") must be set to iPhone SE.')
