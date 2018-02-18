10edfa88ae1b3e9e487dcadb6e9607845d589ddf
Łukasz Mróz <lukasz.mroz@droidsonroids.pl>
2018-02-18 20:14:38 +0100

    Print errors based on reporter

--- xiblint/__main__.py
+++ xiblint/__main__.py
@@ -3,6 +3,7 @@ from xiblint import __version__
 import argparse
 import os
 import sys
+import json
 
 from xiblint.config import Config
 from xiblint.xibcontext import XibContext
@@ -51,24 +52,36 @@ def main():
     #
     # Process paths
     #
-    success = True
+    errors = []
     for root, _, files in os.walk(u'.'):
         for file_path in [os.path.join(root, file) for file in files]:
             checkers = config.checkers(file_path)
-            success = process_file(file_path, checkers) and success
+            errors = errors + process_file(file_path, checkers, reporter)
 
-    sys.exit(0 if success else 1)
+    print_errors(errors, reporter)
 
-
-def process_file(file_path, checkers):
+    sys.exit(0 if errors.count == 0 else 1)
+    
+def process_file(file_path, checkers, reporter):
     _, ext = os.path.splitext(file_path)
     if ext.lower() not in [u".storyboard", u".xib"]:
-        return True
+        return []
     context = XibContext(file_path)
     for rule_name, checker in checkers.items():
         context.rule_name = rule_name
         checker(context)
-    return context.success
+    return context.errors
+
+def print_errors(errors, reporter):
+    if reporter == "raw":
+        for error_dict in errors:
+            print("{}:{}: error: {} [rule: {}]".format(error_dict["file"],
+                error_dict["line"],
+                error_dict["error"],
+                error_dict["rule"]
+            ))
+    elif reporter == "json":
+        print json.dumps(errors)
 
 
 if __name__ == '__main__':
