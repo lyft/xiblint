161dfab983e4e7fdff9ee1b3d8c299362bc5b6f9
Sam Soffes <sam@soff.es>
2018-08-15 18:19:08 -0700

    Add `ignore_localization_comments` option

--- xiblint/rules/strict_font_names.py
+++ xiblint/rules/strict_font_names.py
@@ -8,15 +8,22 @@ class StrictFontNames(Rule):
     Example configuration:
     {
       "allowed_fonts": ["ComicSans-Regular", "ComicSans-Bold"],
-      "allow_system_fonts": true
+      "allow_system_fonts": true,
+      "ignore_localization_comments": true
     }
     """
     def check(self, context):  # type: (Rule, xiblint.xibcontext.XibContext) -> None
         allowed_fonts = self.config.get('allowed_fonts', [])
         allow_system_fonts = self.config.get('allow_system_fonts', False)
+        ignore_localization_comments = self.config.get('ignore_localization_comments', True)
 
         for element in context.tree.findall(".//fontDescription") + context.tree.findall(".//font"):
-            font_name = element.get("name")
+            if ignore_localization_comments and element.tag == 'font':
+                container = element.parent.parent.parent
+                if container.tag == 'attributedString' and container.get('key') == 'userComments':
+                    continue
+
+            font_name = element.get('name')
             if font_name is None:
                 if not allow_system_fonts:
                     context.error(element, "Use of system fonts is not allowed.")
