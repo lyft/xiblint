f353d2835ebbda4e5a28057b19b8c3d365e980e4
Sam Soffes <sam@soff.es>
2019-10-11 08:35:09 -0700

    Compare Xcode colors up to 4 decimal places (#51)
    Xcode formats the floats different in different circumstances. To avoid churn and false positives, only compare the first 4 places of color decimals. This avoids linter errors when the color is inappreciably different.

--- xiblint/rules/color_assets.py
+++ xiblint/rules/color_assets.py
@@ -6,14 +6,20 @@ import json
 
 class ColorAssets(Rule):
     def __init__(self, config):
-        asset_catalog_path = config.get('asset_catalog', None)
+        asset_catalog_path = config.get("asset_catalog", None)
         if asset_catalog_path is None:
-            raise SystemExit("error: Asset catalog not found. Please configure 'asset_catalog'.")
+            raise SystemExit(
+                "error: Asset catalog not found. Please configure 'asset_catalog'.",
+            )
 
         self.assets = glob.glob("{}/**/*.colorset".format(asset_catalog_path))
 
         if not self.assets:
-            raise SystemExit("error: Failed to load asset catalog at: '{}'".format(asset_catalog_path))
+            raise SystemExit(
+                "error: Failed to load asset catalog at: '{}'".format(
+                    asset_catalog_path,
+                ),
+            )
 
         self.colors = {}
 
@@ -25,6 +31,7 @@ class ColorAssets(Rule):
       "asset_catalog": "Resources/Color.xcassets"
     }
     """
+
     def check(self, context):  # type: (XibContext) -> None
         for element in context.tree.findall(".//namedColor"):
             name = element.get("name")
@@ -34,12 +41,18 @@ class ColorAssets(Rule):
 
             expected_color = self._load_color(name)
             if not expected_color:
-                context.error(element, "Named color '{}' is missing from asset catalog.".format(name))
+                context.error(
+                    element,
+                    "Named color '{}' is missing from asset catalog.".format(name),
+                )
                 continue
 
             color_element = element.find("color")
             if color_element is None:
-                context.error(element, "Named color '{}' is missing color definition.".format(name))
+                context.error(
+                    element,
+                    "Named color '{}' is missing color definition.".format(name),
+                )
                 continue
 
             red = color_element.get("red")
@@ -47,13 +60,19 @@ class ColorAssets(Rule):
             blue = color_element.get("blue")
             alpha = color_element.get("alpha")
             if not all([red, green, blue, alpha]):
-                context.error(element, "Named color '{}' has invalid color value.".format(name))
+                context.error(
+                    element, "Named color '{}' has invalid color value.".format(name),
+                )
                 continue
 
             color = (float(red), float(green), float(blue), float(alpha))
 
-            if color != expected_color:
-                context.error(element, "Color value for '{}' does not match asset catalog.".format(name))
+            if not self._compare_colors(color, expected_color):
+                print(color, expected_color)
+                context.error(
+                    element,
+                    "Color value for '{}' does not match asset catalog.".format(name),
+                )
 
     def _load_color(self, name):
         if name in self.colors:
@@ -70,8 +89,12 @@ class ColorAssets(Rule):
                 return color
 
     def _load_components(self, components):
-        return (self._load_component(components, "red"), self._load_component(components, "green"),
-                self._load_component(components, "blue"), self._load_component(components, "alpha"))
+        return (
+            self._load_component(components, "red"),
+            self._load_component(components, "green"),
+            self._load_component(components, "blue"),
+            self._load_component(components, "alpha"),
+        )
 
     def _load_component(self, components, key):
         string = components[key]
@@ -80,3 +103,7 @@ class ColorAssets(Rule):
             return float(string)
 
         return float(string) / 255.0
+
+    def _compare_colors(self, lhs, rhs):
+        # Compare with only 4 decimal places
+        return all(format(l, ".4f") == format(r, ".4f") for l, r in zip(lhs, rhs))
