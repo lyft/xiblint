2e14b5961670c302b611373d54afde10aee6734b
Gonzalo RH <gonzalorehu@gmail.com>
2021-03-03 15:05:14 -0500

    feature(strict_color_names): Added a rule for color validation. (#60)

--- xiblint/rules/named_colors.py
+++ xiblint/rules/named_colors.py
@@ -8,10 +8,14 @@ class NamedColors(Rule):
 
     Example configuration:
     {
+      "allowed_colors": ["CustomRed", "CustomGreen"],
+      "allow_system_colors": true,
       "ignore_alpha": true
     }
     """
     def check(self, context):  # type: (XibContext) -> None
+        allowed_colors = self.config.get('allowed_colors', [])
+        allow_system_colors = self.config.get('allow_system_colors', False)
         ignore_alpha = self.config.get('ignore_alpha', False)
 
         for element in context.tree.findall(".//color"):
@@ -24,16 +28,31 @@ class NamedColors(Rule):
             if element.parent.tag == 'namedColor':
                 continue
 
+            # Skip <color> tags part of a system color definition
+            if element.parent.tag == 'systemColor':
+                continue
+
             # Skip colors with alpha (if configured)
-            if ignore_alpha and element.get('alpha') != '1':
+            if ignore_alpha and element.get('alpha') is not None and element.get('alpha') != '1':
+                continue
+
+            # If `systemColor` or `catalog` is present, it's a named system color
+            if (
+                    element.get('systemColor') is not None or
+                    element.get('cocoaTouchSystemColor') is not None or
+                    element.get('catalog') is not None
+            ):
+                if not allow_system_colors:
+                    context.error(element, "Use of named system colors is not allowed. Use a named color instead.")
                 continue
 
             # Require a name
-            if element.get('name') is None:
+            color_name = element.get('name')
+            if color_name is None:
                 context.error(element, "Use of custom colors is not allowed. Use a named color instead.")
                 continue
 
-            # If `catalog` is present, it's a named system color
-            if element.get('catalog') is not None:
-                context.error(element, "Use of named system colors is not allowed. Use a named color instead.")
-                continue
+            # If allowed_colors is set, verify that color_name is included
+            if allowed_colors and color_name not in allowed_colors:
+                context.error(element, '"{}" is not one of the allowed colors: "{}".'
+                              .format(color_name, allowed_colors))
