372cddbecc8331f8c9dd36706a14d22d7d7e2cec
Keith Smiley <keithbsmiley@gmail.com>
2022-09-13 16:50:20 -0700

    setup.cfg

--- setup.py
+++ setup.py
@@ -1,17 +1,2 @@
-from setuptools import setup, find_packages
-import xiblint
-
-setup(
-    name='xiblint',
-    version=xiblint.__version__,
-    description='Checks .xib and .storyboard files for compliance with best practices',
-    license='apache2',
-    url='https://github.com/lyft/xiblint',
-    author='Ilya Konstantinov',
-    author_email='ikonstantinov@lyft.com',
-    install_requires=[
-        'defusedxml>=0.5.0',
-    ],
-    packages=find_packages(),
-    entry_points={'console_scripts': ['xiblint=xiblint.__main__:main']},
-)
+from setuptools import setup
+setup()
